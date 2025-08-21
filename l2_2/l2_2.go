a6b2cfcafa763970d15998b276b87a9115922e38
Nikita Chernov <rouming44life@gmail.com>
2025-08-21 23:07:59 +0300

    L1.2 is done

---
+++ l2_2/l2_2.go
@@ -0,0 +1,25 @@
+package l2_2
+
+import "sync"
+
+func ConcurrencyCalculate(arr []int) []int {
+	var squares = make([]int, len(arr))
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		for i := 0; i < len(arr); i += 2 {
+			squares[i] = arr[i] * arr[i]
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 1; i < len(arr); i += 2 {
+			squares[i] = arr[i] * arr[i]
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	return squares
+}
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
-import l1_1 "test/l1_1"
+import (
+	"fmt"
+	"runtime"
+	l1_1 "test/l1_1"
+	"test/l2_2"
+)
 
 func main() {
 	// ===== l1.1 ==== //
+	fmt.Println("======= L1.1 =======")
 	//
 	var instance = l1_1.Action{Human: l1_1.Human{"Jessy", 20}}
 	instance.IntroduceYourself()
 	instance.Talk("hi")
 	// ==== end ==== //
+
+	// ===== l1.2 ==== //
+	fmt.Println("L1.2")
+	runtime.GOMAXPROCS(1)
+	arr := []int{2, 4, 6, 8, 10}
+	squaredArr := l2_2.ConcurrencyCalculate(arr)
+	fmt.Println(squaredArr)
 }
