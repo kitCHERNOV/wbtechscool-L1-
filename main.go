17cc06a5002644d532f07c95509bb08eac7e75b7
Nikita Chernov <rouming44life@gmail.com>
2025-08-26 15:46:48 +0300

    l1.4 is done

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ func main() {
 	// ==== end ==== //
 
 	// ===== l1.2 ==== //
-	fmt.Println("L1.2")
+	fmt.Println("======= L1.2 =======")
 	runtime.GOMAXPROCS(1)
 	arr := []int{2, 4, 6, 8, 10}
 	squaredArr := l1_2.ConcurrencyCalculate(arr)
 	fmt.Println(squaredArr)
 
 	// ===== l1.3 ==== //
-	fmt.Println("L1.3 и L1.4")
+	fmt.Println("======= L1.3 и L1.4 =======")
 	numWorkers := rand.Intn(10) + 1
 	l1_3.CreateWorkers(numWorkers)
 
