d36554ccdced089ed3023e4953f8d99ce3852948
Nikita Chernov <rouming44life@gmail.com>
2025-09-03 22:30:00 +0300

    l1.24 is done

--- l1_24/l1_24.go
+++ l1_24/l1_24.go
@@ -9,14 +9,18 @@ type Point struct {
 	x, y float64
 }
 
-func Distance(p, q Point) float64 {
-	xDistance := math.Abs(q.x - p.x)
-	yDistance := math.Abs(q.y - p.y)
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
+func (cp *Point) Distance(other Point) float64 {
+	xDistance := math.Abs(cp.x - other.x)
+	yDistance := math.Abs(cp.y - other.y)
 	return math.Sqrt(xDistance*xDistance + yDistance*yDistance)
 }
 
 func MainFunc() {
-	point1 := Point{x: 10, y: 20}
-	point2 := Point{x: 5, y: 10}
-	fmt.Println(Distance(point1, point2))
+	point1 := NewPoint(10, 20)
+	point2 := NewPoint(20, 30)
+	fmt.Println(point1.Distance(*point2))
 }
